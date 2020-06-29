6269160f5a86a884d2f1032666ae5de0a3d47bef
yamajikosuke <yamajikosuke@users.noreply.github.com>
2020-06-29 11:46:33 +0000

    deploy: 8c39fc31fd9568866975aa5cb71f3bdbfe2b4e66

---
+++ precache-manifest.d51ee2f65f683c1223904d14c8b880c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d8bd91e0f07e2c64c426a04691ffdd",
+    "url": "/react-piano/index.html"
+  },
+  {
+    "revision": "3612b8e9bd9f56e7d5e1",
+    "url": "/react-piano/static/css/main.6c70b1bf.chunk.css"
+  },
+  {
+    "revision": "d00c5f84697fbfe7793e",
+    "url": "/react-piano/static/js/2.b356dcb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-piano/static/js/2.b356dcb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3612b8e9bd9f56e7d5e1",
+    "url": "/react-piano/static/js/main.9e314831.chunk.js"
+  },
+  {
+    "revision": "a08a58cb8338e9e0d716",
+    "url": "/react-piano/static/js/runtime-main.185306cb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41ae879abad69cd71cbc645cb6ab73f3.js"
+  "/react-piano/precache-manifest.d51ee2f65f683c1223904d14c8b880c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-piano/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
