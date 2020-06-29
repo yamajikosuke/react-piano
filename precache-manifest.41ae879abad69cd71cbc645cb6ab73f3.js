649b43f7d100724a68972a0bbc60076abc52c626
yamajikosuke <yamajikosuke@users.noreply.github.com>
2020-06-29 11:35:59 +0000

    deploy: 71d24f3fb0ea302401adf3d9c5571a3455771217

---
+++ precache-manifest.41ae879abad69cd71cbc645cb6ab73f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a078b7cccb93c3671b9ca0ffe2685f20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96dfab81accdb99c972e",
+    "url": "/static/css/main.186ae262.chunk.css"
+  },
+  {
+    "revision": "5782dd89628a7f83236b",
+    "url": "/static/js/2.c670a04f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c670a04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96dfab81accdb99c972e",
+    "url": "/static/js/main.47c7e164.chunk.js"
+  },
+  {
+    "revision": "6f50fd5704f09ba6408f",
+    "url": "/static/js/runtime-main.cb172541.js"
+  }
+]);
